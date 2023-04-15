62e990ebfa6aef0db2e243fcad9cbd643eb27ae2
Bianca Quiroga <128812164+bianquir@users.noreply.github.com>
2023-04-15 19:50:26 -0300

    formulario nombre,apellido,edad

--- Tp1/js/funciones.js
+++ Tp1/js/funciones.js
@@ -1,6 +1,10 @@
 
 function mifuncion(){
-    const aux = document.getElementById("cajatexto");
-    alert(aux.value);
+    const nombre = document.getElementById("nombre");
+    const apellido = document.getElementById("apellido");
+    const edad = document.getElementById("edad");
+    alert(nombre.value);
+    alert(apellido.value);
+    alert(edad.value);
 
 }
