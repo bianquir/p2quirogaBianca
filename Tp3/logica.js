b0c3de866409b061d49c842d812d06cb6580772f
Bianca Quiroga <128812164+bianquir@users.noreply.github.com>
2023-05-25 13:34:48 -0300

    modificaciones

--- Tp3/logica.js
+++ Tp3/logica.js
@@ -3,16 +3,28 @@ function getPerson(){
     .then(function(response){
         return response.json()
     })
-    .then(function(data){
+    .then (function(data){
+        let nombre = (data.results)
+        let fechaN = (data.results[0].dob.date)
         let genero = (data.results[0].gender)
-        let fechan = (data.results[0].dob.date)  
-        let location = (data.results[0].location.city)      
-        let coordinates = (data.results[0].location.coordinates)
         let name = (data.results[0].name.first)
-
-        console.log(genero)
-        console.log (fechan)
-        console.log (location)
-    })
-}
-
+        let apellido = (data.results[0].name.last)
+        let localidad = (data.results[0].location.city)
+        let latitud = (data.results[0].location.coordinates.latitude)
+        let longitud = (data.results[0].location.coordinates.longitude)
+        let ciudad = (data.results[0].location.city)
+        let foto = (data.results[0].picture.large)
+     
+        document.getElementById("foto").src = foto;
+        document.getElementById("foto").style.display = "block";
+        document.getElementById("nombre").innerHTML = "Nombre: " + name;
+        document.getElementById("apellido").innerHTML = "Apellido: " + apellido;
+        document.getElementById("ciudad").innerHTML = "Ciudad: " + ciudad;
+        document.getElementById("fechaNacimiento").innerHTML = "Fecha de nacimiento: " + fechaN;
+        document.getElementById("genero").innerHTML = "Género: " + genero;
+        document.getElementById("latitud").innerHTML = "Latitud: " + latitud;
+        document.getElementById("longitud").innerHTML = "Longitud: " + longitud;
+        document.getElementById("localidad").innerHTML = "Localidad: " + localidad;
+        
+    }
+)}
