ed1b6901cfb1222ce0f09bb3e7c715ec5cd7a1bd
Bianca Quiroga <128812164+bianquir@users.noreply.github.com>
2023-06-15 11:11:05 -0300

    nombre de las localidades
    logre acceder al nombre de las localidades y mostrarlas

--- Practica parcial/js/funciones.js
+++ Practica parcial/js/funciones.js
@@ -12,21 +12,26 @@ function obtenerDatos(){
         const localidades = (data.localidades);
         let nombreProvincia = "no identificada";
         let cantidadMunicipios = 0;
+        let nombreLocalidad = [];
        
 
         localidades.forEach(function(element){
             if (element.provincia.id === id){
                 nombreProvincia= element.provincia.nombre;
                 cantidadMunicipios++;
+                nombreLocalidad.push(element.nombre);
+
             }  
         });
         if (nombreProvincia !== "no identificada") {
-            document.getElementById("nombre").innerHTML = ("provincia : " + nombreProvincia);
-            document.getElementById("municipio").innerHTML = ("cantidad de municipios : " + cantidadMunicipios);
+            document.getElementById("nombre").innerHTML = ("Provincia : " + nombreProvincia);
+            document.getElementById("municipio").innerHTML = ("Cantidad de municipios : " + cantidadMunicipios);
+            document.getElementById("nombreM").innerHTML=("Nombre de los municipios: "+ nombreLocalidad );
 
         }else{
             document.getElementById("nombre").innerHTML= "Id no identificado";
             document.getElementById("municipio").innerHTML= ""; 
+            document.getElementById("nombreM").innerHTML="";
         }
 
     })
